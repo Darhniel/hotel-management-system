2520f393997d5bc009886bd635fecb83673b552c
Darhniel <nnadidarhniel20@gmail.com>
2026-02-13 03:35:50 +0100

    updated build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,8 @@ pip install -r requirements.txt
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
