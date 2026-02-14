8fd6f79835f69da9c5db91403293b5f46bc68bae
Darhniel <nnadidarhniel20@gmail.com>
2026-02-14 14:49:59 +0100

    added staticfiles folder

--- config/settings.py
+++ config/settings.py
@@ -140,6 +140,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'users.User'
